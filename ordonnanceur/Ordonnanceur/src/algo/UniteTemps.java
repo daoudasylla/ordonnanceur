d1c373091cf4f1783cc3050ef7990c1a866857ff
florian4487 <florian4487@hotmail.fr>
2009-02-09 17:03:44 +0000

    création unite de temps

---
+++ ordonnanceur/Ordonnanceur/src/algo/UniteTemps.java
@@ -0,0 +1,22 @@
+package algo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class UniteTemps {
+	
+	//la liste des id des taches dont la période commence sur cette unité
+	private List<Integer> periodes;
+	//l'id de la tache qui occupera cette unité de temps
+	private int idTache;
+	public UniteTemps()
+	{
+		this.periodes = new LinkedList<Integer>();
+	}
+	public int getIdTache() {
+		return idTache;
+	}
+	public void setIdTache(int idTache) {
+		this.idTache = idTache;
+	}
+}
