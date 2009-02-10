ee9d17880f53c5293a5450228f0c138fabdc6b80
florian4487 <florian4487@hotmail.fr>
2009-02-10 15:39:47 +0000

    Aperiodiques completées

--- ordonnanceur/Ordonnanceur/src/exe/RunMe2.java
+++ ordonnanceur/Ordonnanceur/src/exe/RunMe2.java
@@ -3,6 +3,7 @@ package exe;
 import java.util.Random;
 
 import algo.Algorithme;
+import algo.Background;
 import algo.RM;
 import algo.EDF;
 import algo.RM;
@@ -12,6 +13,7 @@ import noyau.Generation;
 import noyau.ListeTaches;
 import noyau.Ordonnanceur;
 import noyau.Periodique;
+import noyau.Tache;
 
 
 public class RunMe2 {
@@ -31,10 +33,11 @@ public class RunMe2 {
       liste = gen.genererTachesPeriodiques(0.5); 
       System.out.println(liste);
       }*/
-      ListeTaches liste2 = new ListeTaches();
-     /* liste2.add(new Periodique(2,6,6));
+      /*ListeTaches liste2 = new ListeTaches();
+     liste2.add(new Periodique(2,6,6));
       liste2.add(new Periodique(2,8,8));
-      liste2.add(new Periodique(2,12,12));*/
+      liste2.add(new Periodique(2,12,12));
+      Ordonnanceur o = new Ordonnanceur(new RM(24),liste2);*/
      /* liste2.add(new Periodique(3,20,20));
       liste2.add(new Periodique(2,10,10));
       liste2.add(new Periodique(2,5,5));
@@ -48,10 +51,18 @@ public class RunMe2 {
       liste2.add(new Periodique(4,10,10));
       liste2.add(new Periodique(2,5,4));
       Ordonnanceur o = new Ordonnanceur(new EDF(20),liste2); */
-      liste2.add(new Periodique(1,4,4));
+      /*liste2.add(new Periodique(1,4,4));
       liste2.add(new Periodique(2,6,6));
       liste2.add(new Periodique(3,8,8));
-      Ordonnanceur o = new Ordonnanceur(new EDF(24),liste2);
+      Ordonnanceur o = new Ordonnanceur(new EDF(24),liste2);*/
+      
+      ////////////TEST BACKGROUND/////////////////////
+      ListeTaches liste2 = new ListeTaches();
+      liste2.add(new Periodique(2,6,6));
+      liste2.add(new Periodique(2,8,8));
+      liste2.add(new Periodique(2,12,12));
+      //liste2.add(new Aperiodique(2));
+       Ordonnanceur o = new Ordonnanceur(new Background(24),liste2);
       o.ordonnancer();
       System.out.println(o);
       
