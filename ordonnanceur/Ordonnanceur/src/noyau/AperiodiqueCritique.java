92aadb4a3b87826868fe5feb1ea82fe807e7f8d4
florian4487 <florian4487@hotmail.fr>
2009-02-25 10:22:43 +0000

    Création et implémentation dans Ordonnanceur d'une méthode de calcule du nombre de préemptions

--- ordonnanceur/Ordonnanceur/src/noyau/AperiodiqueCritique.java
+++ ordonnanceur/Ordonnanceur/src/noyau/AperiodiqueCritique.java
@@ -5,7 +5,13 @@ public class AperiodiqueCritique  extends Aperiodique{
 	private int d;//delai critique
 	public AperiodiqueCritique(int c,int r,int d) {
 		super(c,r);
-		this.d =d;
+		this.setD(d);
+	}
+	public void setD(int d) {
+		this.d = d;
+	}
+	public int getD() {
+		return d;
 	}
 
 }
