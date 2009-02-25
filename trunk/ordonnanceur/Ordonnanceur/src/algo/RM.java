65860a3e92cc26c3c3c19103176e3aedaa74e3c7
florian4487@hotmail.fr <florian4487@hotmail.fr@e2b9ad56-ebcb-11dd-a971-7dbc132099af>
2009-02-25 16:47:14 +0000

    RM fonctionnel en pas a pas

--- trunk/ordonnanceur/Ordonnanceur/src/algo/RM.java
+++ trunk/ordonnanceur/Ordonnanceur/src/algo/RM.java
@@ -1,6 +1,7 @@
 package algo;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
@@ -11,12 +12,29 @@ import noyau.Tache;
 public class RM implements Algorithme{
 	
 	//private int[] ordonnancement;
-	private LinkedList<UniteTemps> ordonnancement;
+	//private LinkedList<UniteTemps> ordonnancement;
+	private Iterator<UniteTemps> iterateur;
+	PriorityQueue<PrioRM> enAttente; //taches en attentes
+	private Periodique tacheEnCours;
 	//private boolean[][] periodes;
 	private ListeTaches liste;
-	private int ppcm;
+	//private int ppcm;
 	private HashMap<Tache,Integer> mapTacheUnitesRestantes;
-	public RM(int ppcm)
+	private int unitesRestantes; //pour la tache en cours
+	public RM() {
+		this.mapTacheUnitesRestantes = new HashMap<Tache,Integer>();
+	}
+	public void initialiser(LinkedList<UniteTemps> ordonnancement, ListeTaches tachesPeriodiques, ListeTaches tachesAperiodiques) {
+		this.enAttente = new PriorityQueue<PrioRM>(); //taches en attentes
+		this.unitesRestantes = 0;
+		this.tacheEnCours = null;
+		this.iterateur = ordonnancement.iterator();
+		this.liste = tachesPeriodiques;
+		for(Tache t : this.liste) {
+			this.mapTacheUnitesRestantes.put(t, new Integer(0));
+		}
+	}
+	/*public RM(int ppcm)
 	{
 		this.ppcm = ppcm;
 		this.ordonnancement = new LinkedList<UniteTemps>();
@@ -24,12 +42,12 @@ public class RM implements Algorithme{
 			this.ordonnancement.add(new UniteTemps(i));
 		this.liste = null;
 		this.mapTacheUnitesRestantes = new HashMap<Tache,Integer>();
-	}
+	}*/
 	
 	/**
 	 * Initialise la liste des débuts de période des tâches
 	 */
-	private void calculePeriodes()
+	/*private void calculePeriodes()
 	{
 		
 		int periodeTemp, periodeTache;
@@ -49,11 +67,63 @@ public class RM implements Algorithme{
 		
 			this.mapTacheUnitesRestantes.put(t, new Integer(0));
 		}
+	}*/
+	public UniteTemps uniteSuivante() {
+
+		//this.calculePeriodes();
+		UniteTemps uniteCourante = this.iterateur.next();
+	
+		
+		for(Tache t :uniteCourante.getPeriodes()){
+			enAttente.add(new PrioRM(((Periodique)t)));
+			
+			if(this.mapTacheUnitesRestantes.get(t)>0) System.out.println("erreur impossible de finir la tache : "+t.getId());
+			else mapTacheUnitesRestantes.put(t,t.getC());
+		}
+		
+		// Si une tache en cours doit etre stoppé car réveil d'une tache prio
+		if(tacheEnCours!=null){
+			Periodique tPrio = enAttente.peek().getTache();
+			if(tPrio != null){
+				
+				if(tacheEnCours.getP() > tPrio.getP()){
+					tacheEnCours = tPrio;
+				}
+			}
+		}
+		
+		//si aucune tache est en cours d'execution on en selectionne une dans la file d'attente
+		if(tacheEnCours == null) {
+			if(enAttente.peek() != null) {// si des taches dans la file d'attente
+				tacheEnCours = enAttente.peek().getTache();					 
+			} else {
+				uniteCourante.setIdTache(0);
+				
+			}
+		}
+		
+		
+		
+		if(tacheEnCours != null) { //si une tache est en cours d'exe
+			uniteCourante.setIdTache(tacheEnCours.getId());
+			
+			
+			
+			unitesRestantes = this.mapTacheUnitesRestantes.get(tacheEnCours)-1;
+			this.mapTacheUnitesRestantes.put(tacheEnCours,unitesRestantes);
+			if(unitesRestantes == 0) {//si c'était la dernière unité de temps
+				enAttente.remove(new PrioRM(tacheEnCours));
+				tacheEnCours = null;
+				
+			}
+		}
+		return uniteCourante;
+		
 	}
-	public LinkedList<UniteTemps> executer(ListeTaches tachesPeriodiques, ListeTaches tachesAperiodiques)
+	/*public LinkedList<UniteTemps> executer(ListeTaches tachesPeriodiques, ListeTaches tachesAperiodiques)
 	{
 		
-		
+		Periodique tacheEnCours = null;
 		this.liste = tachesPeriodiques;
 		Periodique tacheEnCours = null;
 		int unitesRestantes = 0; //pour la tache en cours
@@ -115,6 +185,6 @@ public class RM implements Algorithme{
 
 
 	}
-	
+	*/
 
 }
--- trunk/ordonnanceur/Ordonnanceur/src/exe/RunMe2.java
+++ trunk/ordonnanceur/Ordonnanceur/src/exe/RunMe2.java
@@ -43,10 +43,10 @@ public class RunMe2 {
       Ordonnanceur o = new Ordonnanceur(new RM(24),liste2);
       o.ordonnancer();
       System.out.println(o);*/
-     /*liste2.add(new Periodique(3,20,20));
+     liste2.add(new Periodique(3,20,20));
       liste2.add(new Periodique(2,10,10));
       liste2.add(new Periodique(2,5,5));
-      Ordonnanceur o = new Ordonnanceur(new RM(20),liste2);*/
+      Ordonnanceur o = new Ordonnanceur(new RM(),liste2);
       /*ListeTaches liste2 = new ListeTaches();
      liste2.add(new Periodique(1,4,4));
       liste2.add(new Periodique(2,6,6));
@@ -83,18 +83,18 @@ public class RunMe2 {
       
       ////////////////////TEST POLLING SERVER///////////////////////////
       
-      liste2.add(new Periodique(2,6,6));
+      /*liste2.add(new Periodique(2,6,6));
       liste2.add(new Periodique(1,4,4));
       liste2.add(new Periodique(2,5,5));
       liste2.add(new Aperiodique(2,2));
       liste2.add(new Aperiodique(1,8));
       liste2.add(new Aperiodique(2,12));
       liste2.add(new Aperiodique(1,19));
-       Ordonnanceur o = new Ordonnanceur(new Polling(25),liste2);
-      o.ordonnancer();
+       Ordonnanceur o = new Ordonnanceur(new Polling(25),liste2);*/
+      o.ordonnancer(20);
       System.out.println(o);
-      System.out.println(o.tempsReponseAperiodique());
-      System.out.println(o.nombrePremption());
+      /*System.out.println(o.tempsReponseAperiodique());
+      System.out.println(o.nombrePremption());*/
       
     }
 }
